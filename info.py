25594775e60a8a8715a39145f89c049c3b3cf28d
relangitatayya <relangitatayya@gmail.com>
2025-04-27 07:29:08 +0530

    Update info.py

--- info.py
+++ info.py
@@ -59,7 +59,7 @@ DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in enviro
 
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://relangitatayya:i7S0RgK0mGlZb0La@cluster0.0ywgl2x.mongodb.netRTMoviesDB/?retryWrites=true&w=majority")   # IF Multiple Database Is False Then Fill Only This Database Url.
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://relangitatayya:i7S0RgK0mGlZb0La@cluster0.0ywgl2x.mongodb.net/RTMoviesDB/?retryWrites=true&w=majority")   # IF Multiple Database Is False Then Fill Only This Database Url.
 DATABASE_NAME = environ.get('DATABASE_NAME', "techvjclonefilterbot")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'vjcollection')
 
